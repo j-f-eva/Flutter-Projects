711db69875017d7f30378375c41f0ba250c0e509
Jannatul Ferdous Eva <jannatulfeva17@gmail.com>
2021-08-18 23:52:04 +0600

    Destiny Change
    this is a fun base application by flutter.. Clone & enjoy it ..

---
+++ destini-challenge-starting/lib/story.dart
@@ -0,0 +1,10 @@
+//TODO: Step 2 - Create a new class called Story.
+class Story{
+  String storyTitle;
+  String choice1;
+  String choice2;
+  Story({this.storyTitle, this.choice1, this.choice2});
+  //Story(this.storyTitle,this.choice1,this.choice2,({String storyTitle,String choice1, String choice2}));
+}
+
+//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
